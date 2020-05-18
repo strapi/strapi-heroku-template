0b63024f177a8b73956e31bb9b565d40ea91247d
Maxime Castres <mcastres@student.42.fr>
2020-05-18 17:48:19 +0200

    Add execa

--- scripts/postgres-env.js
+++ scripts/postgres-env.js
@@ -1,5 +1,6 @@
 console.log("Start running postgres-env");
 const fs = require("fs");
+const execa = require("execa");
 
 if (process.env.DATABASE_URL) {
   const databaseConfig = process.env.DATABASE_URL.split("//")[1];
@@ -9,18 +10,30 @@ if (process.env.DATABASE_URL) {
   const newLine = "\r\n";
   let output = "";
 
-  output += `DATABASE_HOST=${host}${newLine}`;
-  output += `DATABASE_PORT=${port}${newLine}`;
-  output += `DATABASE_NAME=${databaseName}${newLine}`;
-  output += `DATABASE_USERNAME=${username}${newLine}`;
-  output += `DATABASE_PASSWORD=${password}${newLine}`;
+  execa("heroku", [
+    "config:set",
+    `DATABASE_HOST=${host}${newLine}`
+  ]).stdout.pipe(process.stdout);
 
-  fs.writeFile(`./.env`, output, err => {
-    if (err) {
-      console.warn("create .env file error: ", err);
-    }
-  });
-  console.log(".env file is generated");
+  execa("heroku", [
+    "config:set",
+    `DATABASE_PORT=${port}${newLine}`
+  ]).stdout.pipe(process.stdout);
+
+  execa("heroku", [
+    "config:set",
+    `DATABASE_NAME=${databaseName}${newLine}`
+  ]).stdout.pipe(process.stdout);
+
+  execa("heroku", [
+    "config:set",
+    `DATABASE_USERNAME=${username}${newLine}`
+  ]).stdout.pipe(process.stdout);
+
+  execa("heroku", [
+    "config:set",
+    `DATABASE_PASSWORD=${password}${newLine}`
+  ]).stdout.pipe(process.stdout);
 }
 
 console.log("Finish running postgres-env");
