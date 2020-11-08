4bb50f06404a9af07edda5530d5ca33c5c2248a0
dmitry <dmitry.a@maxitechs.com>
2020-11-09 00:20:52 +0200

    fix template

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337)
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '503c54d76be328bcf96f24209f243b9c'),
+    },
+  },
 });
