5b1e63c4aafbc4a1639d815a2a72b6781c6723b6
dmitry <dmitry.a@maxitechs.com>
2020-11-12 16:58:35 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db")
+        client: "postgres",
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        ssl: { "rejectUnauthorized": false }
       },
       options: {
         useNullAsDefault: true
@@ -13,3 +18,4 @@ module.exports = ({ env }) => ({
     }
   }
 });
+
