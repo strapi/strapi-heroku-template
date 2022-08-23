386fd598aa6e6de41a81489e869460b633b7e2ce
galihputroaji <109973913+galihputroaji@users.noreply.github.com>
2022-08-23 08:28:48 +0700

    adding loker data

---
+++ src/api/loker/controllers/loker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  loker controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::loker.loker');
---
+++ src/api/loker/routes/loker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * loker router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::loker.loker');
